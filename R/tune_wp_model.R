4f7ee05b5b91cbcfa97cd8d6a7c3b6f9cbab9db2
Ben Baldwin <1425357+guga31bb@users.noreply.github.com>
2020-11-13 08:41:15 -0500

    increment

--- R/tune_wp_model.R
+++ R/tune_wp_model.R
@@ -172,7 +172,7 @@ get_metrics <- function(df, row = 1) {
 
 
 # get results
-results <- map_df(17 : 24, function(x) {
+results <- map_df(25 : 31, function(x) {
   
   gc()
   message(glue::glue("Row {x}"))
